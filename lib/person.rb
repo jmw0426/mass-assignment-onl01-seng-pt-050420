b2cfb9c456dd8cfcd77f316e829def68f983df4a
Joshua Williams <jmw0426@hotmail.com>
2020-06-10 03:20:22 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,7 @@
 class Person
   attr_accessor :name, :birthday, :hair_color
   
-  def initialize(name, birthday, hair_color)
-    
-  end 
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
 end
